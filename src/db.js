ffd33cf3560ab0c83d1ae33b6914bcf57e7bc0c6
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-10-25 20:25:56 -0300

    no se que mas cambiar

--- src/db.js
+++ src/db.js
@@ -1,12 +1,12 @@
 //Creacion de la base de datos
 import { createPool } from "mysql2/promise.js";
-import {
-    DB_HOST,
-    DB_USER,
-    DB_PASSWORD,
-    DB_DATABASE,
-    PORT,
-} from "./config.js"
+// import {
+//     DB_HOST,
+//     DB_USER,
+//     DB_PASSWORD,
+//     DB_DATABASE,
+//     PORT,
+// } from "./config.js"
 
 //con la constante pool se crea la conexion a la base de datos y se manejan las querys
 const pool = createPool({
