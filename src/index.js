a82d06385b450d7725750bf29f3c3d612949b805
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-10-25 19:22:37 -0300

    no se

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import express from "express";
 import employeesRoutes from "./routes/employees.routes.js";
 import indexRoutes from "./routes/index.routes.js";
-import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 app.listen(3306, () => {
   console.log("Server running on port 3306");
