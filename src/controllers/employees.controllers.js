3b423dd704e80cbfd00213bda7f9b513a5ffb455
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-10-24 22:48:22 -0300

    se quito el asincronismo

--- src/controllers/employees.controllers.js
+++ src/controllers/employees.controllers.js
@@ -1,13 +1,13 @@
 import { pool } from "../db.js";
 
-export const getEmployees = async (req, res) => {
-  const [rows] = await pool.query("select * from employee");
+export const getEmployees = (req, res) => {
+  const [rows] = pool.query("select * from employee");
   res.json(rows);
 };
 
-export const getEmployeeById = async (req, res) => {
+export const getEmployeeById = (req, res) => {
   const { id } = req.params;
-  const [rows] = await pool.query("select * from employee where id = ?", [id]);
+  const [rows] = pool.query("select * from employee where id = ?", [id]);
 
   if (rows.length === 0) {
     return res
@@ -18,24 +18,24 @@ export const getEmployeeById = async (req, res) => {
   res.json(rows);
 };
 
-export const createEmployee = async (req, res) => {
+export const createEmployee = (req, res) => {
   const { name, salary } = req.body;
 
   if (!name || !salary) {
     return res.status(400).send("Content not valid");
   }
 
-  const [rows] = await pool.query(
+  const [rows] = pool.query(
     "insert into employee (name, salary) values (?,?)",
     [name, salary]
   );
   res.json({ id: rows.insertId, name, salary });
 };
 
-export const deleteEmployeeById = async (req, res) => {
+export const deleteEmployeeById = (req, res) => {
   const { id } = req.params;
 
-  const [rows] = await pool.query("delete from employee where id = ?", [id]);
+  const [rows] = pool.query("delete from employee where id = ?", [id]);
 
   if (rows.affectedRows === 0) {
     return res
@@ -46,7 +46,7 @@ export const deleteEmployeeById = async (req, res) => {
   res.json({ message: `Employee with id ${id} deleted successfully` });
 };
 
-export const updateEmployeeById = async (req, res) => {
+export const updateEmployeeById = (req, res) => {
   const { id } = req.params;
   const { name, salary } = req.body;
 
@@ -54,7 +54,7 @@ export const updateEmployeeById = async (req, res) => {
     return res.status(400).send("Content not valid");
   }
 
-  const [rows] = await pool.query(
+  const [rows] = pool.query(
     "update employee set name = ifnull(?, name), salary = ifnull(?, salary) where id = ?",
     [name, salary, id]
   );
