c8eae2cbae751fe30d4d5cac3c7d72a51809dd1d
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-10-25 20:32:28 -0300

    port

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import employeesRoutes from "./routes/employees.routes.js";
 import indexRoutes from "./routes/index.routes.js";
 
+import { PORT } from "./config.js"
+
 const app = express();
 
 app.use(express.json());
 
-app.listen(3306, () => {
-  console.log("Server running on port 3306");
+app.listen(PORT, () => {
+  console.log("Server running on port" + PORT);
 });
 
 // Routes
