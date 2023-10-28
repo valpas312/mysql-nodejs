ce2ffefd9d4cb3eb02e08c54eae1fe59ea5bdb5f
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-10-27 22:48:06 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import employeesRoutes from "./routes/employees.routes.js";
 import indexRoutes from "./routes/index.routes.js";
+import cors from "cors";
 
 import { PORT } from "./config.js"
 
 const app = express();
 
+// Middlewares
 app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log("Server running on port" + PORT);
